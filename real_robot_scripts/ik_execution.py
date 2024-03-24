146b74c5a5ba42a471fc7f58c8d11b1851004119
Yifeng Zhu <yifeng.zhu@utexas.edu>
2024-03-24 10:57:29 -0500

    (WIP) Python scripts for real robot evaluation, needs testing

---
+++ real_robot_scripts/ik_execution.py
@@ -0,0 +1,344 @@
+import argparse
+import json
+import os
+import pickle
+import torch
+import threading
+import time
+from pathlib import Path
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+from easydict import EasyDict
+
+from deoxys import config_root
+from deoxys.experimental.motion_utils import follow_joint_traj, reset_joints_to, joint_interpolation_traj
+from deoxys.franka_interface import FrankaInterface
+from deoxys.utils import YamlConfig
+from deoxys.utils.input_utils import input2action
+from deoxys.utils.io_devices import SpaceMouse
+from deoxys.utils.config_utils import robot_config_parse_args
+from deoxys.utils.input_utils import input2action
+from deoxys.utils.io_devices import SpaceMouse
+from deoxys.utils.log_utils import get_deoxys_example_logger
+from deoxys_vision.utils.calibration_utils import load_default_extrinsics, load_default_intrinsics
+from deoxys_vision.utils.camera_utils import assert_camera_ref_convention, get_camera_info
+from deoxys_vision.utils.img_utils import save_depth_in_rgb
+
+import time
+from typing import Union
+
+import numpy as np
+
+from deoxys.utils.config_utils import (get_default_controller_config,
+                                       verify_controller_config)
+
+import init_path
+
+from orion.utils.misc_utils import VideoWriter, get_first_frame_annotation
+from orion.utils.ik_utils import IKWrapper
+from orion.utils.real_robot_utils import RealRobotObsProcessor, ImageCapturer
+from orion.utils.misc_utils import read_from_runtime_file, finish_experiments
+from orion.algos.xmem_tracker import XMemTracker
+
+logger = get_deoxys_example_logger()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--motion-file", type=str)
+    parser.add_argument("--no-record", action="store_true")
+    parser.add_argument("--no-reset", action="store_true")
+    robot_config_parse_args(parser)
+    return parser.parse_args()
+
+
+def reset_joints_to_with_recording(
+    robot_interface,
+    start_joint_pos,
+    controller_cfg: dict = None,
+    timeout=7,
+    gripper_open=False,
+    image_capturer=None,
+    no_record=False,
+):
+    assert type(start_joint_pos) is list or type(start_joint_pos) is np.ndarray
+    if controller_cfg is None:
+        controller_cfg = get_default_controller_config(controller_type="JOINT_POSITION")
+    else:
+        assert controller_cfg["controller_type"] == "JOINT_POSITION", (
+            "This function is only for JOINT POSITION mode. You specified "
+            + controller_cfg["controller_type"]
+        )
+        controller_cfg = verify_controller_config(controller_cfg)
+
+    if gripper_open:
+        gripper_action = -1
+    else:
+        gripper_action = 1
+    if type(start_joint_pos) is list:
+        action = start_joint_pos + [gripper_action]
+    else:
+        action = start_joint_pos.tolist() + [gripper_action]
+    start_time = time.time()
+    while True:
+        if (
+            robot_interface.received_states
+            and robot_interface.check_nonzero_configuration()
+        ):
+            if (
+                np.max(
+                    np.abs(np.array(robot_interface.last_q) - np.array(start_joint_pos))
+                )
+                < 1e-3
+            ):
+                break
+        robot_interface.control(
+            controller_type="JOINT_POSITION",
+            action=action,
+            controller_cfg=controller_cfg,
+        )
+        if image_capturer is not None and not no_record:
+            image_capturer.record_obs()
+        end_time = time.time()
+
+        # Add timeout
+        if end_time - start_time > timeout:
+            break
+
+def hand_move_to_position(robot_interface, device):
+    controller_cfg = YamlConfig(os.path.join(config_root, "compliant-joint-impedance-controller.yml")).as_easydict()
+    controller_type = "JOINT_IMPEDANCE"
+
+    print("Please start moving ... ")
+
+    while True:
+        action, _ = input2action(
+                        device=device,
+                        controller_type="OSC_POSE",
+                    )
+        if action[-1] > 0:
+            break
+        action = list(robot_interface._state_buffer[-1].q) + [-1]
+        robot_interface.control(
+            controller_type=controller_type,
+            action=action,
+            controller_cfg=controller_cfg,
+        )
+
+    print("Moving ended")
+
+
+def ik_execution(args, image_capturer: ImageCapturer):
+    with open("experiments/runtime_T.json", "r") as f:
+        args.motion_file = json.load(f)["file"]
+    runtime_folder, rollout_folder, human_video_annotation_path = read_from_runtime_file()
+    print(f"Run time folder: {runtime_folder}")
+    print(f"Rollout folder: {rollout_folder}")
+    print(f"Human video annotation path: {human_video_annotation_path}")
+
+    # start camera for streaming images
+    observation_cfg = YamlConfig("./configs/real_robot_observation_cfg.yml").as_easydict()
+    observation_cfg.cameras = []
+    for camera_ref in observation_cfg.camera_refs:
+        assert_camera_ref_convention(camera_ref)
+        camera_info = get_camera_info(camera_ref)
+
+        observation_cfg.cameras.append(camera_info)
+
+    image_capturer.get_last_obs()
+
+    # Initialize Franka Interface
+    robot_interface = FrankaInterface(os.path.join(config_root, args.interface_cfg))
+
+    # Initialize spacemouse for terminating program in case of emergency
+    device = SpaceMouse(vendor_id=9583, product_id=50734)
+    device.start_control()
+
+    # start reading robot states until they are received
+    while robot_interface.state_buffer_size == 0:
+        logger.warning("Robot state not received")
+        time.sleep(0.5)
+
+    # hand_move_to_position(robot_interface, device)
+    controller_cfg = YamlConfig(os.path.join(config_root, "joint-impedance-controller.yml")).as_easydict()
+    controller_type = "JOINT_IMPEDANCE"
+
+    last_q = np.array(robot_interface.last_q)
+    initial_q = last_q
+    last_eef_mat, last_eef_pos = robot_interface.last_eef_rot_and_pos
+
+    ik_wrapper = IKWrapper()
+    data = torch.load(args.motion_file)
+    R_seq = data["R_seq"]
+    t_seq = data["t_seq"]
+    target_interaction_centroid = torch.load(args.motion_file)["target_interaction_centroid"]
+    target_interaction_points = torch.load(args.motion_file)["target_interaction_points"]
+
+    logger.info("Starting IK ...")
+    # joint_traj, new_T_seq, quat_seq  = ik_wrapper.ik_trajectory_from_T_seq(T_seq, last_q.tolist())
+
+    z_rotation = data["z_rotation"]
+
+    interaction_vector = target_interaction_points[1] - target_interaction_points[0]
+    interaction_vector /= np.linalg.norm(interaction_vector)
+    z_rotation_2 = np.arccos(
+        np.dot(
+            interaction_vector,
+            np.array([0.0, 1.0, 0.0])
+        )
+    )
+    z_rotation += z_rotation_2
+    z_rotation = (np.pi - z_rotation) % np.pi
+    if z_rotation > np.pi / 2:
+        z_rotation = np.pi - z_rotation
+
+    print("z rotation: ", z_rotation)
+
+    z_rotation = 0
+    pre_rotation_matrix = np.array([
+        [np.cos(z_rotation), -np.sin(z_rotation), 0],
+        [np.sin(z_rotation), np.cos(z_rotation), 0],
+        [0, 0, 1]
+    ])
+
+    pre_pre_joint_seq, _, _ = ik_wrapper.ik_trajectory_from_R_t_seq(
+        [pre_rotation_matrix],
+        [np.zeros((1, 3))],
+        last_q.tolist()
+    )
+    pre_pre_joint_seq = ik_wrapper.interpolate_dense_traj(pre_pre_joint_seq, minimal_displacement=0.03)
+    start_q = pre_pre_joint_seq[-1].tolist()
+
+    # start_q = last_q.tolist()
+    num_points = 50
+    pre_target_location = target_interaction_centroid + np.array([0, 0., 0.07])
+    pre_joint_seq = ik_wrapper.ik_trajectory_to_target_position(
+        np.array(pre_target_location),
+        start_q,
+        num_points=num_points)
+    
+    pre_joint_seq = np.concatenate([pre_pre_joint_seq, pre_joint_seq], axis=0)
+
+    pre_joint_seq_2 = ik_wrapper.ik_trajectory_to_target_position(
+        np.array(target_interaction_centroid),
+        pre_joint_seq[-1].tolist(),
+        num_points=num_points)
+
+    reset_joint_positions = pre_joint_seq_2[-1].tolist()
+    joint_seq, new_T_seq, quat_seq = ik_wrapper.ik_trajectory_from_R_t_seq(R_seq, t_seq, reset_joint_positions)
+    # joint_seq = ik_wrapper.ik_trajectory_to_target_position(np.array([0.4, 0.2, 0.2]), reset_joint_positions)
+
+    joint_seq = ik_wrapper.interpolate_dense_traj(joint_seq)
+    joint_seq = np.array(joint_seq)
+
+    # record_images = []
+    # def record_image():
+    #     color_imgs, depth_imgs = obs_processor.get_original_imgs()
+    #     record_images.append(color_imgs[0])
+
+    minimal_displacement = 0.05
+    visualize_joints = []
+    for joint_traj in [pre_joint_seq, pre_joint_seq_2, joint_seq]:
+        joint_traj = ik_wrapper.interpolate_dense_traj(joint_traj, minimal_displacement=minimal_displacement)
+        logger.info("Visualizing IK results ...")
+        visualize_joints.append(joint_traj)
+    visualize_joints = np.concatenate(visualize_joints, axis=0)
+    ik_wrapper.visualize_joint_sequence(visualize_joints, fps=180)
+
+    dirname = os.path.dirname(args.motion_file)
+          
+    valid_input = False
+    while not valid_input:
+        try:
+            execute = input(f"Excute or not? (enter 0 - No or 1 - Yes)")
+            execute = bool(int(execute))
+            valid_input = True
+        except ValueError:
+            print("Please input 1 or 0!")
+            continue
+
+    if execute:
+        for freespace_joint_seq in [pre_joint_seq, pre_joint_seq_2]:
+            for joint in freespace_joint_seq:
+                action = joint.tolist() + [-1.0]
+                robot_interface.control(
+                    controller_type=controller_type,
+                    action=action,
+                    controller_cfg=controller_cfg,
+                )
+
+                image_capturer.record_obs()
+            
+        for _ in range(10):
+            action = joint_seq[0].tolist() + [1.0]
+            robot_interface.control(
+                controller_type=controller_type,
+                action=action,
+                controller_cfg=controller_cfg,
+            )
+            image_capturer.record_obs()
+        for joint in joint_seq:
+            sp_action, grasp = input2action(
+                        device=device,
+                        controller_type="OSC_POSE",
+                    )
+            if sp_action is None:
+                break
+            
+            action = joint.tolist() + [1.0]
+            robot_interface.control(
+                controller_type=controller_type,
+                action=action,
+                controller_cfg=controller_cfg,
+            )
+            image_capturer.record_obs()
+        if sp_action is not None:
+            for _ in range(10):
+                action = joint_seq[-1].tolist() + [-1.0]
+                robot_interface.control(
+                    controller_type=controller_type,
+                    action=action,
+                    controller_cfg=controller_cfg,
+                )
+                image_capturer.record_obs()
+
+        last_eef_mat, last_eef_pos = robot_interface.last_eef_rot_and_pos
+        post_position = last_eef_pos.reshape(1, 3) + np.array([0.0, 0.0, 0.09])
+        last_q = np.array(robot_interface.last_q)
+
+        post_joint_seq = ik_wrapper.ik_trajectory_to_target_position(
+            np.array(post_position),
+            last_q.tolist(),
+            num_points=num_points)
+        post_joint_seq = ik_wrapper.interpolate_dense_traj(post_joint_seq, minimal_displacement=minimal_displacement)            
+        for post_joint in post_joint_seq:
+            action = post_joint.tolist() + [-1.0]
+            robot_interface.control(
+                controller_type=controller_type,
+                action=action,
+                controller_cfg=controller_cfg,
+            )
+            image_capturer.record_obs()
+
+        if not args.no_reset:
+            reset_joints_to_with_recording(robot_interface, initial_q, gripper_open=True, image_capturer=image_capturer, no_record=args.no_record)
+
+    # record rollout video
+    dirname = os.path.dirname(args.motion_file)
+    with VideoWriter(runtime_folder, "rollout.mp4", save_video=not args.no_record) as video_writer:
+        for rgb_img in image_capturer.record_images:
+            video_writer.append_image(rgb_img)
+    
+    finish_experiments(runtime_folder, rollout_folder, human_video_annotation_path)  
+    robot_interface.close()
+
+
+def main():
+    args = parse_args()
+    image_capturer = ImageCapturer(record=not args.no_record)
+    ik_execution(args, image_capturer)
+
+if __name__ == "__main__":
+    main()
---
+++ real_robot_scripts/whole_execution.py
@@ -0,0 +1,272 @@
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+import json
+import yaml
+import argparse
+from orion.algos.human_video_oogs import HumanVideoOOGs
+from orion.algos.oog import OpenWorldObjectSceneGraph
+from orion.utils.misc_utils import *
+from orion.utils.o3d_utils import *
+from orion.utils.correspondence_utils import CorrespondenceModel
+from orion.utils.real_robot_utils import ImageCapturer
+from orion.utils.log_utils import get_orion_logger
+
+from deoxys import config_root
+from deoxys.franka_interface import FrankaInterface
+from deoxys.utils import YamlConfig
+from deoxys.utils.input_utils import input2action
+from deoxys.utils.io_devices import SpaceMouse
+from deoxys.utils.log_utils import get_deoxys_example_logger
+from deoxys.utils.config_utils import robot_config_parse_args
+
+from deoxys_vision.networking.camera_redis_interface import \
+    CameraRedisSubInterface
+from deoxys_vision.utils.calibration_utils import load_default_extrinsics, load_default_intrinsics
+from deoxys_vision.utils.camera_utils import assert_camera_ref_convention, get_camera_info
+from deoxys_vision.utils.img_utils import save_depth_in_rgb
+
+
+from real_robot_scripts.ik_execution import ik_execution
+
+logger = get_orion_logger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--motion-file", type=str)
+    parser.add_argument("--no-record", action="store_true")
+    parser.add_argument("--no-reset", action="store_true")
+    robot_config_parse_args(parser)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    is_finished = False
+    image_capturer = ImageCapturer(record=not args.no_record)
+    human_video_oogs = HumanVideoOOGs()
+
+    correspondence_model = CorrespondenceModel()
+    human_video_oogs.set_correspondence_model(correspondence_model)
+    
+
+    deviation_angle_threshold = 30
+    skip_trivial_solution = True
+    high_occlusion = False
+    offset = [0.0, -0.02, 0.03]     # system offset due to calibration errors. This offset needs to be adjusted for individual system, but you should only adjust it only once. 
+    velocity_threshold = 1.0
+    target_dist_threshold = 0.01
+    target_intersection_threshold = 100
+
+    runtime_folder, rollout_folder, human_video_annotation_path = read_from_runtime_file()
+
+    robot_video_annotation_path = rollout_folder
+
+    human_video_oogs.generate_from_human_video(human_video_annotation_path)
+    human_video_oogs.plan_inference(velocity_threshold=velocity_threshold,
+                                    target_dist_threshold=target_dist_threshold, 
+                                    target_intersection_threshold=target_intersection_threshold)
+
+    # Algorithm starts
+    while not is_finished:
+        tmp_annotation_path = os.path.join(runtime_folder, "tmp_annotation.png")
+
+        # 1. check OOG matching
+        is_first_frame = True
+        if os.path.exists(tmp_annotation_path):
+            is_first_frame = False
+
+        robot_object_graph = OpenWorldObjectSceneGraph()
+        if is_first_frame:
+            robot_depth_path = os.path.join(robot_video_annotation_path, "depth.png") # "traj_exp/testing/assembly_plane_testing_2_depth.png"
+            robot_first_frame, robot_annotation = get_first_frame_annotation(robot_video_annotation_path)
+
+            last_obs = image_capturer.get_last_obs()
+            robot_object_graph.generate_from_robot_demo(
+                input_image=robot_first_frame,
+                input_depth=load_depth_in_rgb(robot_depth_path),
+                camera_intrinsics=last_obs["intrinsics"],
+                camera_extrinsics=last_obs["extrinsics"],
+                # Comment this line, and the model will first use SAM to get a glboal segmentation mask
+                input_annotation=robot_annotation,
+                reference_graph=human_video_oogs.get_graph(0),
+                is_first_frame=is_first_frame,
+                correspondence_model=human_video_oogs.correspondence_model,
+            )
+            update_annotation = Image.fromarray(robot_object_graph.input_annotation)
+            update_annotation.putpalette(get_palette())
+            update_annotation.save(os.path.join(robot_video_annotation_path, "frame_annotation.png"))
+
+        else:
+            logger.info("loading from previous step")
+            last_obs = image_capturer.get_last_obs()
+            current_image, current_depth = last_obs["color_img"], last_obs["depth_img"]
+            # robot_depth_path = os.path.join(robot_video_annotation_path, "depth.png") # "traj_exp/testing/assembly_plane_testing_2_depth.png"
+
+            tmp_image_path = os.path.join(runtime_folder, "tmp.jpg")
+
+            robot_annotation = np.array(Image.open(tmp_annotation_path))
+            # dataset_name = get_dataset_name_from_annotation(reference_path)
+            # info = load_reconstruction_info_from_human_demo(dataset_name)
+
+            robot_object_graph.generate_from_robot_demo(
+                input_image=current_image[..., ::-1],
+                input_depth=current_depth,
+                camera_intrinsics=last_obs["intrinsics"],
+                camera_extrinsics=last_obs["extrinsics"],
+                # Comment this line, and the model will first use SAM to get a glboal segmentation mask
+                input_annotation=robot_annotation,
+                reference_graph=human_video_oogs.get_graph(0),
+                is_first_frame=is_first_frame,
+                correspondence_model=human_video_oogs.correspondence_model,
+            )
+
+        # human_video_oogs.get_graph(0).draw_overlay_image(mode="all")
+        robot_object_graph.draw_overlay_image(mode="object")
+        # human_video_oogs.get_graph(0).draw_dense_correspondence(robot_object_graph, object_ids=[2])
+
+        robot_object_graph.compute_contact_states(
+            # dist_threshold=0.005, 
+            # intersection_threshold=50
+            )
+        # matched_graph_idx = 3
+        print(human_video_oogs.get_oog_mode_sequence())
+        matched_graph_idx = human_video_oogs.find_matching_oog_idx(robot_object_graph)
+        current_oog_idx = matched_graph_idx
+
+        manipulate_object_id = human_video_oogs.get_manipulate_object_seq()[matched_graph_idx]
+        reference_object_id = human_video_oogs.get_reference_object_seq()[matched_graph_idx]
+        logger.debug(f"manipulate id: {manipulate_object_id}")
+        logger.debug("reference id: {reference_object_id}")
+        
+        logger.info(f"Mathced graph idx is: {matched_graph_idx}")
+        logger.debug(f"Robot current contact state is: {robot_object_graph.contact_states}")
+
+        if matched_graph_idx >= human_video_oogs.num_graphs - 1:
+            logger.info("Finished!")
+            is_finished = True
+            continue
+        # End of checking OOG matching
+
+        # 2. Compute ik from observation
+        subgoal_transform, target_transform = human_video_oogs.compute_subgoal(
+            matched_idx=matched_graph_idx,
+            robot_object_graph=robot_object_graph,
+            manipulate_object_id=manipulate_object_id,
+            reference_object_id=reference_object_id,
+            high_occlusion=high_occlusion,
+            deviation_angle_threshold=deviation_angle_threshold,
+            skip_trivial_solution=skip_trivial_solution
+        )
+
+        R_seq, t_seq,  best_loss, training_data = robot_object_graph.estimate_motion_traj_from_object(
+            object_id=manipulate_object_id, 
+            use_visibility=True,
+            skip_interval=1,
+            # select_subset=3,
+            mode="lie",
+            regularization_weight_pos=0.1,
+            regularization_weight_rot=0.1,    
+            num_max_iter=3,
+            high_occlusion=high_occlusion,
+            optim_kwargs={
+                "lr": 0.01,
+                "num_epochs": 501,
+                "verbose": True,
+                "momentum": 0.9,
+            }
+        )
+
+        logger.debug("target transforma: ", target_transform)
+
+        object_1_pcd_points, object_1_pcd_colors = robot_object_graph.get_objects_3d_points(object_id=manipulate_object_id, remove_outlier_kwargs={"nb_neighbors": 30, "std_ratio": 0.7})
+        object_2_pcd_points, object_2_pcd_colors = robot_object_graph.get_objects_3d_points(object_id=reference_object_id)
+        demo_object_1_pcd_points, demo_object_1_pcd_colors = human_video_oogs.get_graph(current_oog_idx).get_objects_3d_points(object_id=manipulate_object_id)
+        demo_object_2_pcd_points, demo_object_2_pcd_colors = human_video_oogs.get_graph(current_oog_idx).get_objects_3d_points(object_id=reference_object_id)
+
+        logger.debug(f"t_seq information: {t_seq.shape}, sum: {np.sum(t_seq, axis=0)}")
+
+        new_R = np.eye(3)
+        new_R_seq = []
+        R_seq = R_seq
+        for R in R_seq:
+            new_R = R @ new_R
+            new_R_seq.append(new_R)
+        logger.debug(new_R_seq[-1])
+
+        new_points = []
+        new_point = object_1_pcd_points
+        for R, t in zip(R_seq, t_seq):
+            new_point = (R @ (new_point - new_point.mean(axis=0)).T ).T + new_point.mean(axis=0) + t[None, :]
+            # new_point = new_point + t[None, :]
+            new_points.append(new_point)
+
+        def transform_point_clouds(transformation, points):
+            new_points = transformation @ np.concatenate((points, np.ones((points.shape[0], 1))), axis=1).T
+            new_points = new_points[:3, :].T
+            return new_points
+
+        subgoal_points = human_video_oogs.get_graph(current_oog_idx).get_world_trajs(object_ids=[manipulate_object_id])[:, -1, :]
+        subgoal_points = transform_point_clouds(subgoal_transform, subgoal_points)
+
+        object_1_new_points = new_points[-1]
+
+        manipulation_offset = object_1_pcd_points.mean(axis=0) - demo_object_1_pcd_points.mean(axis=0)
+        reference_offset = object_2_pcd_points.mean(axis=0) - demo_object_2_pcd_points.mean(axis=0)
+        new_demo_object_1_pcd_points = transform_point_clouds(target_transform, demo_object_1_pcd_points + manipulation_offset)
+
+        eef_node = human_video_oogs.get_graph(current_oog_idx).eef_node
+        gripper_action = eef_node.get_eef_action(verbose=True)
+
+        interaction_points = eef_node.interaction_affordance.get_interaction_points(include_centroid=False)
+        interaction_points = transform_point_clouds(target_transform, interaction_points + manipulation_offset)
+
+        interaction_offset = np.array(offset).reshape(1, 3)
+        interaction_centroid = eef_node.interaction_affordance.get_affordance_centroid()
+        interaction_centroid = transform_point_clouds(target_transform, interaction_centroid + manipulation_offset)
+        interaction_centroid += interaction_offset
+
+
+        z_rotation = np.arctan2(target_transform[1, 0], target_transform[0, 0])
+
+        logger.debug(f"object_1_new_points mean: {object_1_new_points.mean(axis=0)}, object_1_pcd_points mean: {object_1_pcd_points.mean(axis=0)}")
+        logger.debug("z rotation: ", z_rotation)
+
+        new_demo_object_2_pcd_points = transform_point_clouds(subgoal_transform, demo_object_2_pcd_points + reference_offset)
+
+        # pcd = np.concatenate((
+        #     object_1_pcd_points,
+        #     object_1_new_points, 
+        #     new_demo_object_1_pcd_points,
+        #     object_2_pcd_points,
+        #     new_demo_object_2_pcd_points,
+        #     ), axis=0)
+        # colors = np.concatenate((
+        #     object_1_pcd_colors, 
+        #     object_1_pcd_colors[..., [1, 0, 2]], 
+        #     demo_object_1_pcd_colors[..., [1, 2, 0]] / 2,
+        #     object_2_pcd_colors,
+        #     demo_object_2_pcd_colors[..., ::-1],
+        #     ), axis=0)
+
+        # plotly_fig = plotly_draw_3d_pcd(pcd, colors, addition_points=interaction_points, marker_size=15)
+        # plotly_offline_visualization([plotly_fig], "runtime_visualization.html")
+
+        runtime_T_file = os.path.join(robot_video_annotation_path, f"T_{matched_graph_idx}_seq.pt")
+        torch.save({"R_seq": R_seq, "t_seq": t_seq, 
+                    "target_object_centroid": object_1_pcd_points.mean(axis=0),
+                    "target_interaction_centroid": interaction_centroid,
+                    "target_interaction_points": interaction_points,
+                    "z_rotation": z_rotation
+                    }, runtime_T_file)
+
+        with open("experiments/runtime_T.json", "w") as f:
+            json.dump({"file": runtime_T_file}, f)
+        # Execute motion trajectories
+        ik_execution(args, image_capturer)
+
+
+
+if __name__ == "__main__":
+    main()
